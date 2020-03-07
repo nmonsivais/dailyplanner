e24134b466b1c50e2bc55197c439cc79a537d6ca
Tro <noe.monsivais@gmail.com>
2020-03-07 17:51:04 -0600

    Added initial pseudo code, linked CSS and JS pages from their respective folder.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,23 @@
 
 //add save button
 //
+
+// in order to get one segment or block of time on page...
+// #1 create div with class of row
+
+// #2 first column will contain time via moment (use placeholer to start)
+// #3 second column will contain user INPUT field
+// #4 third column will contain save BUTTON 
+// ---------------------------------------------------------------
+// #5 to capture user information from the INPUT field, you will
+// store their information via a variable (google how to get
+// information from HTML with JavaScript)
+// #6 on the click of save BUTTON, save userinput via local storage
+// - use event listener to listen for button click
+// - google how to save information to local storage
+// - console.log the data once its saved
+// #7 get current time of day via Moment.js to display at top of page
+// to get multiple rows or blocks of time on the
+// page, you would use a for loop
+// focus on styling based off of current time after other functionality
+// is complete
